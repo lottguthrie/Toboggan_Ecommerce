f382a8e1f68197ac51a09944c05db554794b277d
Samuel Guthrie <lottguthrie@yahoo.com>
2021-11-24 10:21:00 -0600

    Initial Application starter code

---
+++ src/components/Apps.jsx
@@ -0,0 +1,14 @@
+import react, { Component } from 'react';
+
+class Appextends extends component {
+    constructor(props){
+        super(props);
+        this.state = {}
+    }
+    render(){
+        return(
+            <h1> Component File</h1>
+
+        )
+    }
+}
