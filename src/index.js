bf934995ff86c6e95e823bbf6d8e4e65ca05cf6d
TylerSerrano98 <91335774+TylerSerrano98@users.noreply.github.com>
2021-11-24 14:40:26 -0600

    register component is coded out, as well as some starter code for our app.jsx file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import react from 'react';
 import reactDom from 'react-dom';
-import App from './components/Apps';
+import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.css';
 
 reactDom.render(<App />, document.getElementById('root'));
