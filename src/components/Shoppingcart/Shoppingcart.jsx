dd7b690fc86c37b82d184a2c99d29daee4f591de
LatoyaNixon <nixonlatoya.ln@gmail.com>
2021-11-30 12:43:06 -0500

    shopping cart

--- src/components/Shoppingcart/Shoppingcart.jsx
+++ src/components/Shoppingcart/Shoppingcart.jsx
@@ -1,19 +1,41 @@
-import React, { Component } from 'react';
-import { Link as RouterLink } from "react-router-dom";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
-const Shoppingcart = ({ items, total, addToShoppingcart, removeFromCShoppingcart }) => {
+const Shoppingcart = (props) => {
+  
+   const { total, removeFromCShoppingcart} = props
+   const [ shopCartData, setShopCartData ] = useState([])
+
+
+   const getProdsByUser = async () => {
+    let jwt = localStorage.getItem("token")
+    
+    try {
+      let response = await axios.get('https://localhost:44394/api/shoppingcart' , {hearders: {Authorization: ' Bearer' + jwt}} ) // add authorization
+      setShopCartData(response.data)
+    }  catch (err) {
+      console.log("Error in api call to shopping cart", err)
+    }
+
+   }
+
+   useEffect(() => {
+    getProdsByUser()
+   }, [])
+   
+
   return (
       <div>
           <h3>Shopping Cart</h3>
           <div className="Shopping Cart">
-                        {items.length > 0 && (
+                        {shopCartData.length > 0 && (
                             <div className="shoppingcart__body">
-                                {items.map(item => (
-                                    <><items key={item.id} {...item} onClick={() => removeFromCShoppingcart(item.id)} /><items key={items.id} {...item} onClick={() => addToShoppingcart(item.id)} /></>
-                                    ))}
+                                {shopCartData}.map(item = (
+                                    <shopCartData key={shopCartData.id} {...shopCartData} onClick={() => removeFromCShoppingcart(shopCartData.id)} />
+                                    ))
                                 </div>
                             )}
-                            {items.length === 0 && (
+                            {shopCartData.length === 0 && (
                                 <div className="alert alert-info">Cart is empty</div>
                             )}
                             <div className="shoppingcart__total">Total: {total} </div>
