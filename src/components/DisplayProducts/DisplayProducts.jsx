1f2d514d00f22ef06788dbd404cb88535fd29661
TylerSerrano98 <91335774+TylerSerrano98@users.noreply.github.com>
2021-11-30 15:54:22 -0600

    merge conflict

--- src/components/DisplayProducts/DisplayProducts.jsx
+++ src/components/DisplayProducts/DisplayProducts.jsx
@@ -2,21 +2,13 @@ import React from 'react';
 import './DisplayProducts.css';
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 23a5d67034228311612f77128aa0d1ea9c50757e
 const DisplayProducts = (props) => {
 
     const { products } = props;
 
         return (
             <div>
-<<<<<<< HEAD
             <table className="Product Display">
-=======
-            <table className="table-container">
->>>>>>> 08d66badc14d2c79627f0e50d465c3be48b1a626
                 <thead>
                     <tr>
                         <th> Name </th>
@@ -26,11 +18,7 @@ const DisplayProducts = (props) => {
                         <th> Review </th>
                     </tr>
                 </thead>
-<<<<<<< HEAD
             
-=======
-                {console.log("Display produtcst props: ", products)}
->>>>>>> 08d66badc14d2c79627f0e50d465c3be48b1a626
                 {products.map((product, index) => {   
                     return (
                         <tbody>
