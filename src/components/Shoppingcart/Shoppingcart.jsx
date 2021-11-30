38af4596e28e94d4f7a8f1ba96322231b7984f08
LatoyaNixon <nixonlatoya.ln@gmail.com>
2021-11-30 12:12:44 -0500

    shoppingcart is working

--- src/components/Shoppingcart/Shoppingcart.jsx
+++ src/components/Shoppingcart/Shoppingcart.jsx
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
 
-const ShoppingCart = ({ items, total, removeFromCart }) => {
+const Shoppingcart = ({ items, total, addToShoppingcart, removeFromCShoppingcart }) => {
   return (
       <div>
           <h3>Shopping Cart</h3>
-
+          <div className="Shopping Cart">
+                        {items.length > 0 && (
+                            <div className="shoppingcart__body">
+                                {items.map(item => (
+                                    <><items key={item.id} {...item} onClick={() => removeFromCShoppingcart(item.id)} /><items key={items.id} {...item} onClick={() => addToShoppingcart(item.id)} /></>
+                                    ))}
+                                </div>
+                            )}
+                            {items.length === 0 && (
+                                <div className="alert alert-info">Cart is empty</div>
+                            )}
+                            <div className="shoppingcart__total">Total: {total} </div>
+                        </div>
+                        </div>
+                        
+                    
+             
+        );
+    }
+                
+            
           
-                      {items.length > 0 && (
-                          <div className="cart__body">
-                              {items.map(item => (
-                                  <item key={item.id} {...item} onClick={() => removeFromCart(item.id)} />
-                              ))}
-                          </div>
-                      )}
-                      {items.length === 0 && (
-                          <div className="alert alert-info">Cart is empty</div>
-                      )}
-                      <div className="cart__total">Total: {total}</div>
-                  </div>
-         
-  );
-  }
 
-  export default ShoppingCart;
+
+  export default Shoppingcart;
 
 
 
